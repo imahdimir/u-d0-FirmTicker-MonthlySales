93eb89dca361f5ef441f049a0ace7d58568813cd
imahdimir <imahdimir@gmail.com>
2022-11-28 20:15:18 +0330

    temp final and output

--- py_modules/_5_apply_modif.py
+++ py_modules/_5_apply_modif.py
@@ -14,6 +14,8 @@ from py_modules._0_get_letters import save_cur_module_temp_data_and_push
 from py_modules._1_get_htmls import ret_gdt_obj_updated_pre_df
 from py_modules._4_read_sales import ColName as PreColName
 from py_modules._4_read_sales import Dirr as PreDirr
+from scipy import stats
+import numpy as np
 
 
 module_n = 5
@@ -29,88 +31,41 @@ class Dirr(PreDirr) :
 dirr = Dirr()
 
 class ColName(PreColName) :
-    isales = 'Int' + super().sales
-    imodif = 'Int' + super().modif
+    isales = 'Int' + PreColName.sales
+    imodif = 'Int' + PreColName.modif
+    nm = 'NextMonth'
+    nm_modif = 'NextMonthModifications'
+    modif_sales = 'ModifiedSales'
 
 cn = ColName()
 
-class XL :
+def read_acc_value_from_col(df , src_col , targ_col) :
+    hc = 'hc'
+    df[hc] = df[src_col].str.replace(',' , '')
 
-    def __init__(self , fp , firm_type) :
-        self.fp = fp
-        self.ft = firm_type
+    pat = '\(\d+\.?\d*\)'
+    msk = df[hc].str.fullmatch(pat)
+    msk = msk.fillna(False)
+    df.loc[msk , hc] = df.loc[msk , hc].str.replace('\(|\)' , '' , regex = True)
+    df.loc[msk , hc] = df.loc[msk , hc].apply(lambda x : '-' + x)
 
-    def read(self) :
-        self.df = pd.read_excel(self.fp , index_col = 0)
+    pat = '-?\d+\.?\d*'
+    msk = df[hc].str.fullmatch(pat)
+    msk = msk.fillna(False)
+    df.loc[msk , targ_col] = df.loc[msk , hc].apply(lambda x : int(float(x)))
 
-    def get_sales_modif_col_names(self) :
-        self.sc , self.mc = ret_sales_modif_cols_by_ft(self.ft)
+    df = df.drop(columns = [hc])
 
-    def get_sales_modif(self) :
-        self.s , self.m = read_sales_modif(self.df , self.sc , self.mc)
+    df[targ_col] = df[targ_col].astype('Int64')
 
-def read_sales_modif(df , sales_col , modif_col) :
-    s = df.loc['SUM' , sales_col]
-    if modif_col in df.columns :
-        m = df.loc['SUM' , modif_col]
-        return s , m
+    return df
+
+def get_next_month(cur_month) :
+    yr , mo = cur_month.split('-')
+    if mo == '12' :
+        return f'{int(yr) + 1}-01'
     else :
-        return s , None
-
-def ret_sales_modif_cols_by_ft(firm_type) :
-    from common import ProductionsCols as PC
-    from common import ServiceCols as SC
-    from common import InsuranceCols as IC
-    from common import LeasingCols as LC
-    from common import RealEstateCols as RC
-    from common import BankCols as BC
-
-    if firm_type == ft.p :
-        return PC.msv , PC.rsv
-    elif firm_type == ft.s :
-        return SC.rjm , SC.rrv
-    elif firm_type == ft.i :
-        return IC.civ , IC.furv
-    elif firm_type == ft.l :
-        return LC.rjmv , LC.riv
-    elif firm_type == ft.r :
-        return RC.jmsv , RC.urcn
-    elif firm_type == ft.b :
-        return BC.jmfr , BC.fufrrv
-
-
-    else :
-        raise ValueError('Invalid firm_type')
-
-@dataclass
-class RT :
-    sales_title: str = None
-    sales: float = None
-    modif: float | None = None
-
-rt = RT()
-
-def targ(fp , firm_type) -> RT :
-    xl = XL(fp , firm_type)
-    xl.read()
-    xl.get_sales_modif_col_names()
-    xl.get_sales_modif()
-    return RT(xl.sc , xl.s , xl.m)
-
-outmap = {
-        cn.sales_t : nameof(rt.sales_title) ,
-        cn.sales   : nameof(rt.sales) ,
-        cn.modif   : nameof(rt.modif) ,
-        }
-
-def filter_2_read(df) :
-    msk = df[cn.patn].notna()
-    print(len(msk[msk]))
-
-    msk = df[cn.fp].apply(lambda fp : Path(fp).exists())
-    print(len(msk[msk]))
-
-    return msk
+        return f'{yr}-{str(int(mo) + 1).zfill(2)}'
 
 def main() :
     pass
@@ -123,26 +78,99 @@ def main() :
     gdt , df = ret_gdt_obj_updated_pre_df(module_n , nc)
 
     ##
-    fu = lambda r : dirr.tbl0 / f'{r[cn.patn]}-{r[c1.TracingNo]}.xlsx'
-    df[cn.fp] = df.apply(fu , axis = 1)
+    df = read_acc_value_from_col(df , cn.sales , cn.isales)
+    df = read_acc_value_from_col(df , cn.modif , cn.imodif)
 
     ##
-    msk = filter_2_read(df)
+    df = df.sort_values(by = c1.PublishDateTime , ascending = False)
 
     ##
-    df = df_apply_parallel(df ,
-                           targ ,
-                           [cn.fp , cn.ft] ,
-                           outmap ,
-                           msk = msk ,
-                           test = False)
+    print(len(df))
+    df = df.drop_duplicates(subset = [c1.CodalTicker , c.jm])
+    print(len(df))
 
     ##
-    df = df.drop(columns = [cn.fp])
+    df[cn.nm] = df[c.jm].apply(get_next_month)
 
     ##
-    for col in [cn.sales , cn.modif] :
-        df[col] = df[col].astype('string')
+    df1 = df[[c1.CodalTicker , c.jm , cn.imodif]]
+    df1 = df1.rename(columns = {
+            c.jm      : cn.nm ,
+            cn.imodif : cn.nm_modif
+            })
+
+    df = df.merge(df1 , on = [c1.CodalTicker , cn.nm] , how = 'left')
+
+    ##
+    df[cn.nm_modif] = df[cn.nm_modif].fillna(0)
+
+    ##
+    df[cn.modif_sales] = df[cn.isales] + df[cn.nm_modif]
+
+    ##
+    msk = df[cn.isales].isna()
+    msk |= df[cn.isales].eq(0)
+
+    print(len(df))
+    df = df[~msk]
+    print(len(df))
+
+    ##
+    msk = df[cn.modif_sales].eq(0)
+
+    print(len(df))
+    df = df[~msk]
+    print(len(df))
+
+    ##
+    z_sc = stats.zscore(df[cn.modif_sales].astype(float))
+
+    abs_z = np.abs(z_sc)
+    msk = abs_z < 3
+
+    print(len(df))
+    df = df[msk]
+    print(len(df))
+
+    ##
+    c2k = {
+            c1.CodalTicker : None ,
+            c.jm           : None ,
+            cn.modif_sales : None ,
+            }
+
+    df = df[list(c2k.keys())]
+
+    ##
+    df = df.rename(columns = {
+            cn.modif_sales : 'ModifiedSales(MRial)'
+            })
+
+    ##
+    df = df.sort_values(c.jm , ascending = False)
+
+    ##
+    df.to_excel('temp.xlsx' , index = False)
+
+    ##
+    msk = df[c1.CodalTicker].eq('افق')
+    print(len(df[msk]))
+
+    ##
+    df1 = df[msk]
+
+    ##
+    df1.to_excel('t1.xlsx' , index = False)
+
+    ##
+
+    ##
+
+    ##
+
+    ##
+
+    ##
 
     ##
     save_cur_module_temp_data_and_push(gdt , module_n , df)
